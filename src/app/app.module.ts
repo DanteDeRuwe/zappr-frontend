500605f60304aa0f73c794c6f1a93641d649d4a3
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-03-29 11:34:56 +0200

    Some cleanup and formatting and corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DiscoverComponent } from './discover/discover.component';
-import { SeriesShowcaseComponent } from './series-showcase/series-showcase.component';
-import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DiscoverComponent } from "./discover/discover.component";
+import { SeriesShowcaseComponent } from "./series-showcase/series-showcase.component";
+import { GraphQLModule } from "./graphql.module";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DiscoverComponent,
-    SeriesShowcaseComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    GraphQLModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, DiscoverComponent, SeriesShowcaseComponent],
+  imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
