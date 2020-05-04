d78b4549aae30c24ea101ab49a8785a67e39e7e1
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-05-04 16:58:12 +0200

    Basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DiscoverComponent } from "./series/discover/discover.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "discover", component: DiscoverComponent },
+  { path: "", redirectTo: "discover", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
