422282e0c06b14e84eb9755b4884b2b0261d1f6b
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-05-18 19:03:56 +0200

    Added user model

---
+++ src/app/users/user.model.ts
@@ -0,0 +1,11 @@
+import { Series } from "../series/series.model";
+
+export interface User {
+  id: number;
+  fullName: string;
+  email: string;
+  favoriteSeries: Series[];
+  ratedSeries: Series[];
+  watchedEpisodes: any[]; //for now
+  watchListedSeries: any[]; //for now
+}
