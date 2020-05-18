ef6ded0d4ed9e1885104f3cd43fafb2ccc4adc77
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-05-18 20:42:11 +0200

    Adding favorite and watchlisted series

--- src/app/series/series-actions/series-actions.component.ts
+++ src/app/series/series-actions/series-actions.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Series } from "../series.model";
 import { SeriesdataService } from "../seriesdata.service";
 import { UsersdataService } from "src/app/users/usersdata.service";
+import { Observable } from "rxjs";
+import { User } from "src/app/users/user.model";
 
 @Component({
   selector: "series-actions",
@@ -10,16 +12,30 @@ import { UsersdataService } from "src/app/users/usersdata.service";
 })
 export class SeriesActionsComponent implements OnInit {
   @Input() series: Series;
+  public result: Observable<User>;
+  private _user: User;
 
   constructor(private _dataService: UsersdataService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._dataService
+      .getBasicInfo$(1) //todo, user is hardcoded
+      .subscribe((x) => (this._user = x));
+  }
 
   addSeriesToWatchList() {
-    this._dataService.addSeriesToWatchList(this.series);
+    this.result = this._dataService.addSeriesToWatchList(
+      this.series,
+      this._user
+    );
+    this.result.subscribe((x) => (this._user = x)); //subscribe or the mutation wont go through
   }
 
   addSeriesToFavorites() {
-    this._dataService.addSeriesToFavorites(this.series);
+    this.result = this._dataService.addSeriesToFavorites(
+      this.series,
+      this._user
+    );
+    this.result.subscribe((x) => (this._user = x)); //subscribe or the mutation wont go through
   }
 }
--- src/app/users/usersdata.service.ts
+++ src/app/users/usersdata.service.ts
@@ -4,18 +4,68 @@ import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Apollo, QueryRef } from "apollo-angular";
 import { Series } from "../series/series.model";
+import { User } from "./user.model";
 
 @Injectable({
   providedIn: "root",
 })
 export class UsersdataService {
-  constructor() {}
+  constructor(private _apollo: Apollo) {}
 
-  addSeriesToWatchList(series: Series) {
-    console.log(`You tried adding ${series.name} to watchlist`);
+  getBasicInfo$(id: number): Observable<User> {
+    const QUERY = gql`
+    query{
+      userQuery{
+        get(id: ${id}){
+          email, fullName, id
+        }
+      }
+    }
+    `;
+    return this._apollo
+      .watchQuery<any>({ query: QUERY })
+      .valueChanges.pipe(map((s) => s.data.userQuery.get));
   }
 
-  addSeriesToFavorites(series: Series) {
-    console.log(`You tried adding ${series.name} to favorites`);
+  addSeriesToWatchList(series: Series, user: User): Observable<User> {
+    const MUTATION = gql`
+      mutation addSeriesToWatchList($userid: Int!, $seriesid: Int!) {
+        userMutation {
+          addSeriesToWatchList(userId: $userid, seriesId: $seriesid) {
+            id
+          }
+        }
+      }
+    `;
+    return this._apollo
+      .mutate<any>({
+        mutation: MUTATION,
+        variables: {
+          userid: user.id,
+          seriesid: series.id,
+        },
+      })
+      .pipe(map((s) => s.data.userMutation.addSeriesToWatchList));
+  }
+
+  addSeriesToFavorites(series: Series, user: User): Observable<User> {
+    const MUTATION = gql`
+      mutation addFavoriteSeries($userid: Int!, $seriesid: Int!) {
+        userMutation {
+          addFavoriteSeries(userId: $userid, seriesId: $seriesid) {
+            id
+          }
+        }
+      }
+    `;
+    return this._apollo
+      .mutate<any>({
+        mutation: MUTATION,
+        variables: {
+          userid: user.id,
+          seriesid: series.id,
+        },
+      })
+      .pipe(map((s) => s.data.userMutation.addSeriesToFavorites));
   }
 }
