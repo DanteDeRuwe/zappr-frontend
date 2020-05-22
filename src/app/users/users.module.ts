98a1dc4a19200d9c05cff61e138f65d75eb3db23
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-05-22 10:46:03 +0200

    Add link to register in login

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { LoginComponent } from "./login/login.component";
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from "./register/register.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
 })
 export class UsersModule {}
